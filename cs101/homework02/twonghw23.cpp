219dd3ffbe8096903f799fd188ce1a123ebbd17e
Tony Wong <tony@idlepattern.com>
2015-03-01 10:25:33 -0800

    adding layout

---
+++ cs101/homework02/twonghw23.cpp
@@ -0,0 +1,16 @@
+/*
+The formulas for calculating BMI are
+BMI =
+weightInPounds  703 over
+heightInInches  heightInInches
+Create a BMI calculator application that reads the users weight in pounds and height in feet inches,
+then calculates and displays the users body mass index. Also, the application should display the
+following information from the Department of Health and Human Services/National Institutes of
+Health so the user can evaluate his/her BMI:
+1
+BMI VALUES
+Underweight: less than 18.5
+Normal: between 18.5 and 24.9
+Overweight: between 25 and 29.9
+Obese: 30 or greater
+*/
---
+++ cs101/homework02/twonghw24.cpp
@@ -0,0 +1,11 @@
+/*
+
+Write a program that nds the equation of the line determined by two given points. For the
+two points you should use 4 variables double x1, y1, x2, y2; Once the user enters these values,
+the program continues by determining the equation of the line passing through (x1, y1) and (x2,
+y2).
+Note, that there are two cases:
+1. Slope is undened (vertical line, i.e. x1 == x2)
+2. Slope is dened. In the latter case, you will also have to compute the y-intercept. Use double
+m, b; for slope and y-intercept respectively.
+The code should print the equation of the line.*/
