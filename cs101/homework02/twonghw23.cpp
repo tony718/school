f4e38daf0c21edad61817d9768e4e2da132d78a9
Tony Wong <tony@idlepattern.com>
2015-03-06 11:53:06 -0500

    pushing

--- cs101/homework02/twonghw23.cpp
+++ cs101/homework02/twonghw23.cpp
@@ -1,16 +1,37 @@
 /*
-The formulas for calculating BMI are
-BMI =
-weightInPounds  703 over
-heightInInches  heightInInches
-Create a BMI calculator application that reads the users weight in pounds and height in feet inches,
-then calculates and displays the users body mass index. Also, the application should display the
-following information from the Department of Health and Human Services/National Institutes of
-Health so the user can evaluate his/her BMI:
-1
-BMI VALUES
+
+
+   BMI VALUES
 Underweight: less than 18.5
 Normal: between 18.5 and 24.9
 Overweight: between 25 and 29.9
 Obese: 30 or greater
+
 */
+
+#include <iostream>
+using namespace std;
+int main(){
+    double weight,height,BMI;
+    string BMIvalue = "\n Your BMI value indicate you are ";
+    cout << " Enter your weight in pounds : " ;
+    cin >> weight;
+    cout << " Enter your height in inches : " ;
+    cin >> height;
+
+    BMI = (weight*703) / (height*height);
+
+    cout << BMI << endl;
+
+    if ( BMI >= 30 )
+        cout << BMIvalue << "obese." << endl;
+    else if ( BMI <= 29.9 && BMI >= 25)
+        cout << BMIvalue << "overweight." << endl;
+    else if ( BMI <= 24.9 && BMI >= 18.5 ) 
+        cout << BMIvalue << "normal." << endl;
+    else
+        cout << BMIvalue << "underweight." << endl;
+
+
+
+}
