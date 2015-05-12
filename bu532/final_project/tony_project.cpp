039a5d1913ec2a469fda9bda7d77c192086b20de
Tony Wong <tony@Tonys-Mac-Pro.local>
2015-05-12 07:29:45 -0400

    saving

---
+++ bu532/final_project/tony_project.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+#include <fstream>
+#include <string>
+#include <sstream>
+using namespace std;
+
+int main(){
+	
+	vector<vector <string> > namedb(10,vector<string>(10));
+
+	cout << "current size of namedb = " << namedb.size() << endl;
+		
+	for( unsigned int i=0 ; i < 10 ; i++){
+		namedb[i][1] = "Lebron";
+		namedb[i][2] = "James";
+
+}
+	for( unsigned int i=0 ; i < 10 ; i++){
+		cout << namedb[i][1] << "\t" << namedb[i][2] << endl;
+			 }
+
+	namedb[0][3][2]="hello";
+	cout << namedb[3][2][2] << endl;
+}
